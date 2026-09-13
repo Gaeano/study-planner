06a5a30a2335b4ba3bf4d309b9daf6748144950b
Gaeano <francesannagaeamutia@gmail.com>
2026-09-13 12:34:05 +0800

    feat: highlight correct page in the sidebar

--- app/Layout_Components/sidebar.tsx
+++ app/Layout_Components/sidebar.tsx
@@ -1,22 +1,25 @@
 'use client';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 
 export default function Sidebar(){
+
+    const pathname = usePathname();
 
     return (
     <aside className="w-64 bg-zinc-950 border-r border-zinc-800 p-6 flex flex-col gap-6">
         <h2 className="text-xl font-bold text-purple-600">Mini Study Planner</h2>
         <nav className="flex flex-col gap-4">
-        <Link href="/dashboard" className="font-medium text-purple-500">
+        <Link href="/dashboard" className={`font-medium ${pathname === '/dashboard' ? 'text-purple-500' : 'text-zinc-400 hover:text-purple-400'} transition`}>
             Dashboard
         </Link>
-        <Link href="/tasks" className="font-medium text-zinc-400 hover:text-purple-400 transition">
+        <Link href="/tasks" className={`font-medium ${pathname === '/tasks' ? 'text-purple-500' : 'text-zinc-400 hover:text-purple-400'} transition`}>
             All Tasks
         </Link>
-        <Link href="/pomodoro" className="font-medium text-zinc-400 hover:text-purple-400 transition">
+        <Link href="/pomodoro" className={`font-medium ${pathname === '/pomodoro' ? 'text-purple-500' : 'text-zinc-400 hover:text-purple-400'} transition`}>
             Pomodoro Timer
         </Link>
-        </nav>
-    </aside>
+            </nav>
+        </aside>
     );
 }
